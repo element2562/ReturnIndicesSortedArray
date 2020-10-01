8a4f3b0a6f6efbe15a28e2b5851963734875b009
element2562 <element25668@gmail.com>
2020-10-01 15:51:51 -0500

    initial and final commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CodingChallenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 2, 2, 2, 3, 4, 5, 6, 6, 7, 8 };
+            Console.WriteLine(FirstAndLastIndices(arr, 2));
+        }
+
+        static int[] FirstAndLastIndices(int[] sortedArr, int target)
+        {
+            int firstIndex = 0;
+            int lastIndex = 0;
+            for (int i = 0; i < sortedArr.Length; i++)
+            {
+                if (sortedArr[i] == target)
+                {
+                    firstIndex = i;
+                    break;
+                }
+            }
+            for (int j = firstIndex; j < sortedArr.Length; j++)
+            {
+                if (sortedArr[j] != target && sortedArr[j - 1] == target)
+                {
+                    lastIndex = j - 1;
+                    break;
+                }
+            }
+            return new int[] { firstIndex, lastIndex };
+        }
+    }
+}
